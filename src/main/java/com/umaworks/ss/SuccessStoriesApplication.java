66b0496a25f092829cf248d68d8076f275a797e8
Anuj Singh <anujsingh@Anujs-MBP.lan1>
2019-12-19 11:40:53 -0600

    This is the first commit

---
+++ src/main/java/com/umaworks/ss/SuccessStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.umaworks.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccessStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuccessStoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umaworks/ss/SuccessStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umaworks.ss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuccessStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
